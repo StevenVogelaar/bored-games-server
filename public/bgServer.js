d984cde3e13b47f3a6a2348915eda06fe1f9b7e0
Steven <steven.vogelaar@gmail.com>
2020-05-15 14:14:41 -0600

    Updates.

--- public/bgServer.js
+++ public/bgServer.js
@@ -1,7 +1,5 @@
 const BoredGamesServer = require("../lib/BoredGamesServer.js");
-const fs = require('fs');
-
-const configPath = './config.json'
+const ConfigManager = require("../lib/ConfigManager");
 
 process.on('SIGTERM', () => {
 	console.log("Process terminating ...");
@@ -14,8 +12,8 @@ process.on('SIGINT', () => {
 })
 
 
-openConfig();
-const config = readConfig();
+ConfigManager.openConfig();
+const config = ConfigManager.readConfig();
 
 
 
@@ -25,52 +23,4 @@ const server = new BoredGamesServer(config);
 server.run();
 
 
-function openConfig(){
-
-	try {
-
-		fs.openSync(configPath, fs.constants.O_CREAT, fs.constants.S_IWUSR);
-		
-	} catch (err){
-	
-		console.log(err);
-		process.kill(process.pid, 'SIGTERM');
-	}
-}
-
-/**
- * See writeDefaultConfig for all valid config contents.
- */
-function readConfig(){
-
-	let config;
-
-	try {
-		config = JSON.parse(fs.readFileSync(configPath));
-	} catch (err){
-		if (err instanceof SyntaxError){
-			config = {};
-		} else {
-			throw err;
-		}
-	}
-	
-	if (config.version === undefined){
-		writeDefaultConfig();
-		return readConfig();
-	}
-
-	return config;
-}
-
-function writeDefaultConfig(){
-	const config = {
-		version: 1.0,
-		dbConnectString: 'mongodb://localhost:27017',
-		port: 3820,
-		address: '127.0.0.1'
-	};
-
-	fs.writeFileSync(configPath, JSON.stringify(config));
-}
 
